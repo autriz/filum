297d09ca162ffcb239c5b1a31de58b801a553c0a
autriz <explayinformer8519@gmail.com>
2024-11-22 10:18:56 +0400

    initial layout cleanup, added theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="bg-purple-800">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="min-h-screen w-screen"></div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,22 @@
 import type { Config } from 'tailwindcss';
 
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
+import * as themes from '@skeletonlabs/skeleton/themes';
+import theme from './theme';
+
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		contentPath(import.meta.url, 'svelte')
+	],
 	darkMode: "class",
 	theme: {
 		extend: {}
 	},
 
-	plugins: []
+	plugins: [
+		skeleton({
+			themes: [ theme ]
+		})
+	]
 } satisfies Config;
