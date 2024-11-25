23e70d68b62e06e9c8ae74cba4fb9a19d9511b33
autriz <explayinformer8519@gmail.com>
2024-11-26 00:01:37 +0400

    slightly changed landing page

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -4,7 +4,7 @@
 		aria-hidden="true"
 	>
 		<div
-			class="pulse relative left-[calc(50%-11rem)] aspect-[16/9] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] lg:w-[120rem]"
+			class="pulse relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem] lg:w-[120rem]"
 			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 		></div>
 	</div>
@@ -43,11 +43,11 @@
 		</svg>
 		<div class="text-center">
 			<h1
-				class="text-balance text-5xl font-semibold tracking-tight text-warning-400-600 sm:text-7xl"
+				class="text-balance text-5xl font-semibold tracking-tight text-tertiary-400-600 sm:text-7xl"
 			>
-				Header
+				Filum
 			</h1>
-			<p class="mt-8 text-pretty text-lg font-medium text-surface-950-50 sm:text-xl/8">
+			<p class="mt-8 text-pretty text-lg font-medium sm:text-xl/8">
 				Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
 				Elit sunt amet fugiat veniam occaecat.
 			</p>
