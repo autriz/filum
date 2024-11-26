84820a2601c781fcdfd9369869e2a26fddcc5f71
autriz <explayinformer8519@gmail.com>
2024-11-27 01:16:29 +0400

    search query WIP

--- src/routes/api/service/+server.ts
+++ src/routes/api/service/+server.ts
@@ -1,7 +1,7 @@
 import { db } from '$lib/server/db/index.js';
-import { service } from '$lib/server/db/schema.js';
+import { business, service } from '$lib/server/db/schema.js';
 import { json } from '@sveltejs/kit';
-import { like, or } from 'drizzle-orm';
+import { eq, like, or, sql } from 'drizzle-orm';
 
 export async function GET({ url }) {
     const query = url.searchParams.get("q");
@@ -10,16 +10,31 @@ export async function GET({ url }) {
         // || query.length < 3 add later
     ) return json([]);
 
-    const services = await db.select({ 
+    const sqlQuery = db.select({ 
         id: service.id, 
         businessId: service.businessId, 
         name: service.name, 
+        businessName: business.name,
         price: service.price 
     }).from(service)
+    .innerJoin(business, eq(business.id, service.businessId))
+    // Работает, но чуствителен к регистру
     .where(or(
         like(service.name, `%${query}%`), 
-        like(service.description, `%${query}%`)
-    ));
+        like(service.description, `%${query}%`),
+        like(business.name, `%${query}%`)
+    ))
+    // Работает, должен быть не чуствителен к регистру, но нет
+    // lower(x) не работает
+    // .where(
+    //     sql.raw(`((lower(service.name) like lower('%${query}%')) or (lower(service.description) like lower('%${query}%')) or (lower(business.name) like lower('%${query}%')))`,
+    // ));
+
+    console.log(sqlQuery.getSQL().toQuery({ escapeName: (name) => name, escapeParam: (num, value) => value as string, escapeString: (str) => str }).sql);
+
+    const services = await sqlQuery;
+
+    console.log(services);
 
     return json(services);
 }
