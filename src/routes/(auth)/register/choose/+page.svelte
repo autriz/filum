87c47e89bdd30b0fdf00f86ac57b7ca81f68abd0
SmerchProsto <smerchprosto@gmail.com>
2024-11-28 23:58:11 +0400

    fix

---
+++ src/routes/(auth)/register/choose/+page.svelte
@@ -0,0 +1,17 @@
+
+<div
+	class="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 transition-colors"
+>
+	<div
+		class=" rounded-lg p-6 w-full max-w-md bg-white dark:bg-gray-800 transition-colors"
+		style="box-shadow: 0px -2px 62px 19px rgba(34, 60, 80, 0.2)"
+	>
+		<h1 class="text-2xl font-semibold text-center text-gray-800 dark:text-gray-200 mb-6">Выберите тип регистрации:</h1>
+		<div class="space-y-4">
+			<a href="/register/choose/business" class="text-blue-500 dark:text-blue-400 hover:underline">Зарегистрироваться как компания</a>
+			<a href="/register/choose/user" class="text-blue-500 dark:text-blue-400 hover:underline">Зарегистрироваться как пользователь</a>
+		</div>
+
+	</div>
+</div>
+
