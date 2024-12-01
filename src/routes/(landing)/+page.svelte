cc108d96279cfcdb0d972ef3441d99974c0b038a
autriz <explayinformer8519@gmail.com>
2024-12-01 19:27:16 +0400

    fixed styles for centering search bar

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -2,7 +2,7 @@
 	import Search from '$lib/components/Search.svelte';
 </script>
 
-<main class="relative isolate h-full overflow-hidden px-6 pt-14 lg:px-8">
+<main class="relative isolate h-full overflow-hidden px-6 lg:px-8">
 	<div
 		class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 lg:-top-60"
 		aria-hidden="true"
@@ -12,7 +12,7 @@
 			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
 		></div>
 	</div>
-	<div class="m-auto max-w-2xl py-64">
+	<div class="m-auto flex h-full max-w-2xl items-center justify-center">
 		<svg aria-hidden="true" class="mask absolute inset-0 -z-10 h-full w-full stroke-surface-600/30">
 			<defs>
 				<pattern
@@ -33,7 +33,7 @@
 				stroke-width="0"
 			></rect>
 		</svg>
-		<div class="text-center">
+		<div class="w-full text-center">
 			<h1
 				class="select-none text-balance text-5xl font-semibold tracking-tight text-tertiary-400-600 sm:text-7xl"
 			>
