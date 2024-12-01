f5281e2e60730d5b65055e8467d102fa237f8fa2
autriz <explayinformer8519@gmail.com>
2024-12-01 17:48:52 +0400

    updated shortening algorithm

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -5,11 +5,20 @@
 
 	let { src = $bindable(''), alt = $bindable(''), class: className, ...props }: Props = $props();
 
-	let shortenedAlt = alt
-		?.split(' ')
-		.map((s) => s[0])
-		.join('')
-		.toUpperCase();
+	let shortenedAlt = $state('');
+
+	if (alt.length > 0) {
+		let words = alt.split(' ');
+
+		if (words.length > 1) {
+			let symbols = words.map((s) => s[0]);
+			let firstAndLastSymbols = [symbols.at(0), symbols.at(-1)];
+
+			shortenedAlt = firstAndLastSymbols.join('').toUpperCase();
+		} else {
+			shortenedAlt = words.at(0)![0].toUpperCase();
+		}
+	}
 
 	const {
 		elements: { image, fallback }
