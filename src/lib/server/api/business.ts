4cabafaa31348f11847153191e155553cc549573
autriz <explayinformer8519@gmail.com>
2024-12-04 01:26:27 +0400

    formatting

--- src/lib/server/api/business.ts
+++ src/lib/server/api/business.ts
@@ -1,44 +1,43 @@
-import { eq } from "drizzle-orm";
-import { db } from "../db";
+import { eq } from 'drizzle-orm';
+import { db } from '../db';
 import { randomUUID } from 'crypto';
-import { business, type Business, type BusinessInsert } from "../db/schema";
-import { service } from "../db/schema";
+import { business, type Business, type BusinessInsert } from '../db/schema';
+import { service } from '../db/schema';
 
 export async function getBusinesses() {
-    return await db.select()
-        .from(business)
-        .limit(15);
+	return await db.select().from(business).limit(15);
 }
 
 export async function getBusiness(id: string) {
-    return await db.select()
-        .from(business)
-        .where(eq(business.id, id));
+	return await db.select().from(business).where(eq(business.id, id));
 }
 
 export async function createBusiness(newBusiness: BusinessInsert) {
-    const newBusinessData = {
-        ...newBusiness,
-        id: randomUUID()
-    };
-    await db.insert(business).values(newBusinessData);
-    return newBusinessData;
+	const newBusinessData = {
+		...newBusiness,
+		id: randomUUID()
+	};
+	await db.insert(business).values(newBusinessData);
+	return newBusinessData;
 }
 
-export async function updateBusiness(id: string, updatedFields: Partial<Omit<Business, "type" | "id">>) {
-    return (await db.update(business)
-        .set(updatedFields)
-        .returning()
-        .where(eq(business.id, id))).at(0);
+export async function updateBusiness(
+	id: string,
+	updatedFields: Partial<Omit<Business, 'type' | 'id'>>
+) {
+	let updatedBusiness = await db
+		.update(business)
+		.set(updatedFields)
+		.returning()
+		.where(eq(business.id, id));
+
+	return updatedBusiness.at(0);
 }
 
 export async function deleteBusiness(id: string) {
-    return await db.delete(business)
-        .where(eq(business.id, id));
+	return await db.delete(business).where(eq(business.id, id));
 }
 
 export async function getBusinessServices(businessId: string) {
-    return await db.select()
-        .from(service)
-        .where(eq(service.businessId, businessId));
+	return await db.select().from(service).where(eq(service.businessId, businessId));
 }
